7988c639524edc7a73362c16b852c83740662101
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-07-20 15:14:10 +0200

    test

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+let testMsg = '2 secondes après le start'
+ 
+console.log('Avant le start')
+start()
+console.log('Après le start')
+ 
+function getMsg() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(testMsg)
+        }, 2000)
+    })
+}
+ 
+async function start() {
+    console.log(await getMsg())
+}
