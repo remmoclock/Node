dae10ce31aa4d026898f90ed0f985cedd71b73a1
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-07-25 00:34:28 +0200

    npm morgan

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/books', (req, res) => {
+  res.send('All Books!')
+})
+app.get('/books/:id', (req, res) => {
+  res.send('1 Book!')
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
