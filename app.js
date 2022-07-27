c10630b1785cc3a6c6c1af5d167d371ba103dd98
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-07-27 13:47:42 +0200

    set config variables

--- app.js
+++ app.js
@@ -5,202 +5,201 @@ const morgan = require("morgan");
 const config = require("./config");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nodejs",
+  host: config.db.host,
+  database: config.db.database,
+  user: config.db.user,
+  password: config.db.password,
 });
 
 db.connect((err) => {
-
-  if (err)
-      console.log(err.message)
+  if (err) console.log(err.message);
   else {
+    console.log("Connected.");
 
-      console.log('Connected.')
+    const app = express();
 
-      const app = express()
+    let MembersRouter = express.Router();
 
-      let MembersRouter = express.Router()
+    app.use(morgan("dev"));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-      app.use(morgan('dev'))
-      app.use(bodyParser.json());
-      app.use(bodyParser.urlencoded({ extended: true }));
+    MembersRouter.route("/:id")
 
-      MembersRouter.route('/:id')
-
-          // Récupère un membre avec son ID
-          .get((req, res) => {
-
-              db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
-                  if (err) {
-                      res.json((err.message))
-                  } else {
-
-                      if (result[0] != undefined) {
-                          res.json((result[0]))
-                      } else {
-                          res.json(('Wrong id'))
-                      }
-
-                  }
-              })
-
-          })
-
-          // Modifie un membre avec ID
-          .put((req, res) => {
-
-              if (req.body.name) {
-
-                  db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
-                      if (err) {
-                          res.json((err.message))
-                      } else {
-
-                          if (result[0] != undefined) {
-
-                              db.query('SELECT * FROM members WHERE name = ? AND id != ?', [req.body.name, req.params.id], (err, result) => {
-                                  if (err) {
-                                      res.json((err.message))
-                                  } else {
-
-                                      if (result[0] != undefined) {
-                                          res.json(('same name'))
-                                      } else {
-
-                                          db.query('UPDATE members SET name = ? WHERE id = ?', [req.body.name, req.params.id], (err, result) => {
-                                              if (err) {
-                                                  res.json((err.message))
-                                              } else {
-                                                  res.json((true))
-                                              }
-                                          })
-
-                                      }
-
-                                  }
-                              })
-
-                          } else {
-                              res.json(('Wrong id'))
-                          }
-
-                      }
-                  })
-
+      // Récupère un membre avec son ID
+      .get((req, res) => {
+        db.query(
+          "SELECT * FROM members WHERE id = ?",
+          [req.params.id],
+          (err, result) => {
+            if (err) {
+              res.json(err.message);
+            } else {
+              if (result[0] != undefined) {
+                res.json(result[0]);
               } else {
-                  res.json(('no name value'))
+                res.json("Wrong id");
               }
+            }
+          }
+        );
+      })
 
-          })
-
-          // Supprime un membre avec ID
-          .delete((req, res) => {
-
-              db.query('SELECT * FROM members WHERE id = ?', [req.params.id], (err, result) => {
-                  if (err) {
-                      res.json((err.message))
-                  } else {
-
-                      if (result[0] != undefined) {
-
-                          db.query('DELETE FROM members WHERE id = ?', [req.params.id], (err, result) => {
+      // Modifie un membre avec ID
+      .put((req, res) => {
+        if (req.body.name) {
+          db.query(
+            "SELECT * FROM members WHERE id = ?",
+            [req.params.id],
+            (err, result) => {
+              if (err) {
+                res.json(err.message);
+              } else {
+                if (result[0] != undefined) {
+                  db.query(
+                    "SELECT * FROM members WHERE name = ? AND id != ?",
+                    [req.body.name, req.params.id],
+                    (err, result) => {
+                      if (err) {
+                        res.json(err.message);
+                      } else {
+                        if (result[0] != undefined) {
+                          res.json("same name");
+                        } else {
+                          db.query(
+                            "UPDATE members SET name = ? WHERE id = ?",
+                            [req.body.name, req.params.id],
+                            (err, result) => {
                               if (err) {
-                                  res.json((err.message))
+                                res.json(err.message);
                               } else {
-                                  res.json((true))
+                                res.json(true);
                               }
-                          })
-
-                      } else {
-                          res.json(error('Wrong id'))
+                            }
+                          );
+                        }
                       }
+                    }
+                  );
+                } else {
+                  res.json("Wrong id");
+                }
+              }
+            }
+          );
+        } else {
+          res.json("no name value");
+        }
+      })
 
+      // Supprime un membre avec ID
+      .delete((req, res) => {
+        db.query(
+          "SELECT * FROM members WHERE id = ?",
+          [req.params.id],
+          (err, result) => {
+            if (err) {
+              res.json(err.message);
+            } else {
+              if (result[0] != undefined) {
+                db.query(
+                  "DELETE FROM members WHERE id = ?",
+                  [req.params.id],
+                  (err, result) => {
+                    if (err) {
+                      res.json(err.message);
+                    } else {
+                      res.json(true);
+                    }
                   }
-              })
-
-          })
-
-      MembersRouter.route('/')
-
-          // Récupère tous les membres
-          .get((req, res) => {
-              if (req.query.max != undefined && req.query.max > 0) {
-
-                  db.query('SELECT * FROM members LIMIT 0, ?', [req.query.max], (err, result) => {
-                      if (err) {
-                          res.json(error(err.message))
-                      } else {
-                          res.json((result))
-                      }
-                  })
-
-              } else if (req.query.max != undefined) {
-                  res.json(error('Wrong max value'))
+                );
               } else {
-
-                  db.query('SELECT * FROM members', (err, result) => {
-                      if (err) {
-                          res.json(error(err.message))
-                      } else {
-                          res.json((result))
-                      }
-                  })
-
+                res.json(error("Wrong id"));
               }
-          })
+            }
+          }
+        );
+      });
 
-          // Ajoute un membre avec son nom
-          .post((req, res) => {
+    MembersRouter.route("/")
 
-              if (req.body.name) {
+      // Récupère tous les membres
+      .get((req, res) => {
+        if (req.query.max != undefined && req.query.max > 0) {
+          db.query(
+            "SELECT * FROM members LIMIT 0, ?",
+            [req.query.max],
+            (err, result) => {
+              if (err) {
+                res.json(error(err.message));
+              } else {
+                res.json(result);
+              }
+            }
+          );
+        } else if (req.query.max != undefined) {
+          res.json(error("Wrong max value"));
+        } else {
+          db.query("SELECT * FROM members", (err, result) => {
+            if (err) {
+              res.json(error(err.message));
+            } else {
+              res.json(result);
+            }
+          });
+        }
+      })
 
-                  db.query('SELECT * FROM members WHERE name = ?', [req.body.name], (err, result) => {
+      // Ajoute un membre avec son nom
+      .post((req, res) => {
+        if (req.body.name) {
+          db.query(
+            "SELECT * FROM members WHERE name = ?",
+            [req.body.name],
+            (err, result) => {
+              if (err) {
+                res.json(error(err.message));
+              } else {
+                if (result[0] != undefined) {
+                  res.json(error("name already taken"));
+                } else {
+                  db.query(
+                    "INSERT INTO members(name) VALUES(?)",
+                    [req.body.name],
+                    (err, result) => {
                       if (err) {
-                          res.json(error(err.message))
+                        res.json(error(err.message));
                       } else {
-
-                          if (result[0] != undefined) {
-                              res.json(error('name already taken'))
-                          } else {
-
-                              db.query('INSERT INTO members(name) VALUES(?)', [req.body.name], (err, result) => {
-                                  if (err) {
-                                      res.json(error(err.message))
-                                  } else {
-
-                                      db.query('SELECT * FROM members WHERE name = ?', [req.body.name], (err, result) => {
-
-                                          if (err) {
-                                              res.json(error(err.message))
-                                          } else {
-                                              res.json(({
-                                                  id: result[0].id,
-                                                  name: result[0].name
-                                              }))
-                                          }
-
-                                      })
-
-                                  }
-                              })
-
+                        db.query(
+                          "SELECT * FROM members WHERE name = ?",
+                          [req.body.name],
+                          (err, result) => {
+                            if (err) {
+                              res.json(error(err.message));
+                            } else {
+                              res.json({
+                                id: result[0].id,
+                                name: result[0].name,
+                              });
+                            }
                           }
-
+                        );
                       }
-                  })
-
-              } else {
-                  res.json(('no name value'))
+                    }
+                  );
+                }
               }
+            }
+          );
+        } else {
+          res.json("no name value");
+        }
+      });
 
-          })
+    app.use(config.rootAPI + "members", MembersRouter);
 
-      app.use(config.rootAPI+'members', MembersRouter)
-
-      app.listen(config.port, () => console.log('Started on port '+config.port))
-
+    app.listen(config.port, () =>
+      console.log("Started on port " + config.port)
+    );
   }
-
-})
+});
