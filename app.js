08cd6d8bd8bd5af333b357da7e061b3c489ef50b
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-07-27 15:01:24 +0200

    install promise sql

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("promise-mysql");
 const bodyParser = require("body-parser");
 const express = require("express");
 const morgan = require("morgan");
